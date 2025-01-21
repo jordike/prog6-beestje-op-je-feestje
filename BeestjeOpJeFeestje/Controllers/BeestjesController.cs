28b0a8898b9bb653f369548ffff01430a0aa738a
Wessenator <deheuswessel@gmail.com>
2025-01-21 16:57:30 +0100

    feat: added classes

---
+++ BeestjeOpJeFeestje/Controllers/BeestjesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeestjeOpJeFeestje.Controllers
+{
+    public class BeestjesController : Controller
+    {
+        // TODO: add authorization to this controller to prevent unauthorized access. only Owners of the farm should be able to access this controller.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
