e94f8d38e728cf03c2f42071cadefe55ff95b265
Wessenator <deheuswessel@gmail.com>
2025-01-21 16:42:41 +0100

    feature: setup entity framework

---
+++ BeestjeOpJeFeestje/Models/BeestjeOpJeFeestjeContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BeestjeOpJeFeestje.Models
+{
+    public class BeestjeOpJeFeestjeContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        public BeestjeOpJeFeestjeContext()
+        {
+        }   
+
+        public BeestjeOpJeFeestjeContext(DbContextOptions<BeestjeOpJeFeestjeContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
