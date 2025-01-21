07cb8863cea539bfdf3e98d743adbb10d34f516d
Wessenator <deheuswessel@gmail.com>
2025-01-21 16:43:10 +0100

    fix: forgot to add program.cs

--- BeestjeOpJeFeestje/Program.cs
+++ BeestjeOpJeFeestje/Program.cs
@@ -1,35 +1,44 @@
-namespace BeestjeOpJeFeestje;
+    using BeestjeOpJeFeestje.Models;
+    using Microsoft.EntityFrameworkCore;
 
-public class Program
-{
-    public static void Main(string[] args)
+    namespace BeestjeOpJeFeestje;
+
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<BeestjeOpJeFeestjeContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("BeestjeOpJeFeestje"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
     }
-}
