ab031b631340e1be5cc2c3a94b29d401ac20f65c
Jordi Keijzers <jordikeijzers@gmail.com>
2025-01-22 14:19:45 +0100

    feat: add basic login form

--- BeestjeOpJeFeestje/Controllers/AuthController.cs
+++ BeestjeOpJeFeestje/Controllers/AuthController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeestjeOpJeFeestje.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeestjeOpJeFeestje.Controllers;
 public class AuthController : Controller
 {
     public IActionResult Login()
+    {
+        LoginViewModel viewModel = new LoginViewModel();
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel viewModel)
     {
         return View();
     }
---
+++ BeestjeOpJeFeestje/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeestjeOpJeFeestje.Models.ViewModels;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(50)]
+    public string Password { get; set; }
+}
