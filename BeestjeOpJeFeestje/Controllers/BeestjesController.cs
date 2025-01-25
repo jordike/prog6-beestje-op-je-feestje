56deedb411f91d35a694f132a4435d6dfe9865aa
Wessenator <deheuswessel@gmail.com>
2025-01-25 10:50:22 +0100

    feat: finished beestje crud

--- BeestjeOpJeFeestje/Controllers/BeestjesController.cs
+++ BeestjeOpJeFeestje/Controllers/BeestjesController.cs
@@ -1,13 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeestjeOpJeFeestje.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeestjeOpJeFeestje.Controllers
 {
     public class BeestjesController : Controller
     {
         // TODO: add authorization to this controller to prevent unauthorized access. only Owners of the farm should be able to access this controller.
+        public BeestjeOpJeFeestjeContext _context;
+
+        public BeestjesController(BeestjeOpJeFeestjeContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var animals = GetAnimals();
+            return View(animals);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Animal());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Animal animal)
+        {
+            _context.Animals.Add(animal);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Read(int id)
+        {
+            Animal animal = GetAnimal(id);
+
+            List<Booking>? bookings = GetAnimalBookings(animal);
+
+            ViewData["bookings"] = bookings;
+            return View(animal);
+        }
+
+        public IActionResult Update(int id)
+        {
+            Animal animal = GetAnimal(id);
+            return View(animal);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Animal animal)
+        {
+            _context.Animals.Update(animal);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            Animal animal = GetAnimal(id);
+            return View(animal);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Animal animal)
+        {
+            _context.Animals.Remove(animal);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public List<Animal> GetAnimals() 
+        {
+            List<Animal> animals = _context.Animals.ToList();
+            return animals;
+        }
+
+        public Animal GetAnimal(int id)
+        {
+            Animal animal = _context.Animals.FirstOrDefault(a => a.Id == id);
+            return animal;
+        }
+
+        public List<Booking> GetAnimalBookings(Animal animal)
+        {
+            List<Booking> bookings = _context.Bookings.Where(b => b.animals.Contains(animal)).ToList();
+            return bookings;
         }
     }
 }
---
+++ BeestjeOpJeFeestje/Migrations/20250121162432_animalTestData.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeestjeOpJeFeestje.Migrations
+{
+    /// <inheritdoc />
+    public partial class animalTestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "BookingId", "ImageURL", "Name", "Price", "Type" },
+                values: new object[,]
+                {
+                    { 1, null, "https://example.com/aap.jpg", "Aap", 50f, "Jungle" },
+                    { 2, null, "https://example.com/olifant.jpg", "Olifant", 200f, "Jungle" },
+                    { 3, null, "https://example.com/zebra.jpg", "Zebra", 150f, "Jungle" },
+                    { 4, null, "https://example.com/leeuw.jpg", "Leeuw", 300f, "Jungle" },
+                    { 5, null, "https://example.com/hond.jpg", "Hond", 30f, "Boerderij" },
+                    { 6, null, "https://example.com/ezel.jpg", "Ezel", 60f, "Boerderij" },
+                    { 7, null, "https://example.com/koe.jpg", "Koe", 120f, "Boerderij" },
+                    { 8, null, "https://example.com/eend.jpg", "Eend", 20f, "Boerderij" },
+                    { 9, null, "https://example.com/kuiken.jpg", "Kuiken", 10f, "Boerderij" },
+                    { 10, null, "https://example.com/pinguin.jpg", "Pinguïn", 80f, "Sneeuw" },
+                    { 11, null, "https://example.com/ijsbeer.jpg", "IJsbeer", 250f, "Sneeuw" },
+                    { 12, null, "https://example.com/zeehond.jpg", "Zeehond", 100f, "Sneeuw" },
+                    { 13, null, "https://example.com/kameel.jpg", "Kameel", 180f, "Woestijn" },
+                    { 14, null, "https://example.com/slang.jpg", "Slang", 70f, "Woestijn" },
+                    { 15, null, "https://example.com/t-rex.jpg", "T-Rex", 1000f, "VIP" },
+                    { 16, null, "https://example.com/unicorn.jpg", "Unicorn", 1200f, "VIP" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 16);
+        }
+    }
+}
